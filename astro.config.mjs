d56aa0389b6a9ed0d503d66913ce2e996293d64f
Manjot Saggu <91143015+msaggu204@users.noreply.github.com>
2025-03-18 20:24:44 -0600

    Setup Astro with Tailwind

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://msaggu204.github.io', // Your GitHub Pages URL
+  base: '/msaggu204.github.io/',
+  output: 'static',
+});
