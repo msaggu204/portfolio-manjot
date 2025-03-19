342e4aa51ce0a4073eece2f519fb6e5996fa6b25
Manjot Saggu <91143015+msaggu204@users.noreply.github.com>
2025-03-18 21:29:57 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://msaggu204.github.io',
-    base: '/msaggu204.github.io/',
-  });
+  site: 'https://msaggu204.github.io',
+  base: '/',
+});
