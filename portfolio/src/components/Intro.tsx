9c76b26c0870c8652aa017dca6f8a4cefe0985dd
Manjot Saggu <91143015+msaggu204@users.noreply.github.com>
2025-03-30 19:35:35 -0600

    Restructuring

---
+++ portfolio/src/components/Intro.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Intro: React.FC = () => {
+  return (
+    <>
+      <div className="intro">
+        <h1>Hi there, my name is</h1>
+      </div>
+    </>
+  );
+};
+
+export default Intro;
